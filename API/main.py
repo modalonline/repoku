cf21dc4f7a02e810fbb0fb2897881e5cb808361a
LAPTOP-67TNSMIJ\Damas <online.dnwdms@gmail.com>
2021-06-01 22:06:52 +0700

    Add function to insert data to the database (POST) and function for error handler (return error message)

--- API/main.py
+++ API/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flaskext.mysql import MySQL
 import pymysql
 app = Flask(__name__)
@@ -33,6 +33,59 @@ def dataset():
     finally:
         cursor.close()
         conn.close()
+
+@app.route('/input', methods=['POST'])
+def add_data():
+    conn = None
+    cursor = None
+    try:
+        _json = request.json
+        _provinsi = _json['provinsi']
+        _kabkota = _json['kabkota']
+        _kecamatan = _json['kecamatan']
+        _desa = _json['desa']
+        _nik = _json['nik']
+        _nama = _json['nama']
+        _penghasilan = _json['penghasilan']
+        _jmltj = _json['jmltj']
+        _umur = _json['umur']
+        _ibuhamil = _json['ibuhamil']
+        _lansia = _json['lansia']
+        _disable = _json['disable']
+        _jmlmtr = _json['jmlmtr']
+        _jmlmob = _json['jmlmob']
+        _bpnt = _json['bpnt']
+        _bst = _json['bst']
+        _pkh = _json['pkh']
+        if _provinsi and _kabkota and _kecamatan and _desa and _nik and _nama and _penghasilan and _jmltj and _umur and _ibuhamil and _lansia and _disable and _jmlmtr and _jmlmob and _bpnt and _bst and _pkh and request.method == 'POST':
+            sql = "INSERT INTO dataset(Provinsi, KabKota, Kecamatan, Desa, NIK, Nama, PenghasilanBulanan, JumlahTanggungJawab, Umur, IbuHamil, LanjutUsia, Disabilitas, JumlahMotor, JumlahMobil, BPNT, BST, PKH) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            data = (_provinsi, _kabkota, _kecamatan, _desa, _nik, _nama, _penghasilan, _jmltj, _umur, _ibuhamil, _lansia, _disable, _jmlmtr, _jmlmob, _bpnt, _bst, _pkh)
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(sql, data)
+            conn.commit()
+            resp = jsonify('Data added successfully!')
+            resp.status_code = 200
+            return resp
+        else:
+            return not_found()
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
 
 if __name__ == '__main__':
     app.run()
